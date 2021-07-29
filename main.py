fe5c79ec23f201d6ef305eac843d7db94f253291
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-07-29 09:10:24 -0400

    Question 5: Omitting country exported to .csv file works

--- main.py
+++ main.py
@@ -12,19 +12,20 @@ if not path.exists(poppulo_csv_file):
 
 
 ####################################################################################################################
-#  2. Programmatically create a subdirectory named year-month-date where YMD is specific to the day program is run #
+# 2. Programmatically create a subdirectory named year-month-date where YMD is specific to the day program is run #
 ####################################################################################################################
 dir_name = datetime.today().strftime('%Y-%m-%d')
 
 try:
     os.mkdir(dir_name)
+    print("QUESTION #2:  " + os.getcwd() + "/" + dir_name + " directory was created.")
 except OSError as folder_error:
     print(folder_error)
 
 
-# #################################################################################################
-#  3. Generate a text file, named "headers.txt", containing a string of the provided headers only #
-# #################################################################################################
+##################################################################################################
+# 3. Generate a text file, named "headers.txt", containing a string of the provided headers only #
+##################################################################################################
 headers_txt_file = "headers.txt"
 
 try:
@@ -38,15 +39,15 @@ try:
                 else:
                     headers_outfile.write(value + ",")
 
-            print("Header information gathered from: " + poppulo_csv_file + " file was:", header_row_from_csv)
+            print("QUESTION #3:  Header information gathered from: " + poppulo_csv_file + " file was:", header_row_from_csv)
 
 except OSError as file_error:
     print(file_error)
 
 
-# ######################################################################################################
-#  4. Generate a separate CSV file per 'Department' value found within the provided CSV file, name     #
-#     the CSV file 'departmentValue.csv', where departmentValue is the actual Department value itself. #
+#######################################################################################################
+# 4. Generate a separate CSV file per 'Department' value found within the provided CSV file, name     #
+#    the CSV file 'departmentValue.csv', where departmentValue is the actual Department value itself. #
 ########################################################################################################
 
 try:
@@ -57,10 +58,25 @@ try:
     for each_dept in dept_column:
         out_file = open(each_dept + ".csv", "w+")
 
-    print(dept_column)
+    print("QUESTION #4:  The following .csv files were created: ", dept_column)
 
 except OSError as file_error:
     print(file_error)
 
 
+#######################################################################################################
+#  5. Generate a separate CSV file named 'no_countries.csv', where the 'Country' field is             #
+#     omitted from the file entirely.                                                                 #
+#######################################################################################################
 
+try:
+    country_omitted_filename = "no_countries.csv"
+    cols_to_use = ["Email", "Surname", "FirstName", "Department"]
+    country_omitted = pd.read_csv(poppulo_csv_file, usecols=cols_to_use)
+    country_omitted.to_csv(country_omitted_filename, sep=',', index=False)  # index=False req'd to eliminate prepending a ',' to data set
+
+    print("QUESTION #5:  " + country_omitted_filename + " has been created with the following data")
+    print(country_omitted)
+
+except OSError as file_error:
+    print(file_error)
