a06029dbaeac81f04e5c981446e8dfaa93419990
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-07-31 19:48:11 -0400

    Found small bug that was not including the first line of exporting of csv files to child xml files

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# Some other ideas that would take longer to implement but would be more efficient:
+# Some other ideas that would take longer to implement could prove useful:
 #   a) Break some operations out into functions to pass parameters into.
 #   b) When writing xml template, instead of repeating writing of the same data in different sections,
 #      create a function to do this instead which will some redundancy in code.
+#   c) Check to see if files exist in case and handle accordingly
 
 
 import csv
 import os
 from os import path
 from datetime import datetime
-import pandas as pd  # pandas module for ease of use to navigate individual column data in csv files
-import base64
+import pandas as pd            # pandas module for ease of use to navigate individual column data in csv files
+import base64                  # base64 module to encode department csv files and embed into their respective xml files
 
 poppulo_csv_file = "poppulo_techtask.csv"
 delimiter = ","
-
 
 if not path.exists(poppulo_csv_file):
     print(os.getcwd() + "/" + poppulo_csv_file + " does not exist and program cannot proceed.")
@@ -38,6 +38,7 @@ except OSError as folder_error:
 ##################################################################################################
 headers_txt_file = "headers.txt"
 
+# Using the built in csv_reader for python for experimentation versus pandas csv module
 try:
     with open(headers_txt_file, "w+") as headers_outfile:
         with open(poppulo_csv_file, 'r') as csv_readfile:
@@ -59,7 +60,8 @@ except OSError as file_error:
 #######################################################################################################
 # 4. Generate a separate CSV file per 'Department' value found within the provided CSV file, name     #
 #    the CSV file 'departmentValue.csv', where departmentValue is the actual Department value itself. #
-########################################################################################################
+#######################################################################################################
+
 try:
     csv_readfile = pd.read_csv(poppulo_csv_file)
     dept_column = csv_readfile.Department
@@ -117,14 +119,14 @@ try:
     out_file.write("\t</tags>\n")
     out_file.write("\t<subscriber_data>\n")
 
-    # Using the headers previously extracted from question #3
+    # Using the header previously extracted from question #3
     out_file.write("\t\t<columns>" + delimiter.join(header_row_from_csv) + "</columns>\n")
 
     out_file.write("\t\t<skip_first_line>true</skip_first_line>\n")
     out_file.write("\t\t<field_separator>comma</field_separator>\n")
     out_file.write("\t\t<data>\n")
 
-    # Using the headers previously extracted from question #3
+    # Using the header previously extracted from question #3
     out_file.write("\t\t\t" + delimiter.join(header_row_from_csv) + "\n")
 
     # Write data to master_XML_filename ('master.xml') from csv_readfile ('poppulu_techtask.csv')
@@ -134,6 +136,13 @@ try:
         for each_row in csv_reader:
             out_file.write("\t\t\t" + delimiter.join(each_row) + "\n")
 
+    #######################################################################################################
+    # FROM QUESTION 8: embed the master csv file in the data section
+    #######################################################################################################
+    with open(poppulo_csv_file, "rb") as master_csv_data:
+        master_csv_encoded = base64.b64encode(master_csv_data.read())
+
+    out_file.write("\t\t\t<embed>" + str(master_csv_encoded.decode('utf-8')) + "</embed>\n")
     out_file.write("\t\t</data>\n")
     out_file.write("\t</subscriber_data>\n")
     out_file.write("</subscriber_import_job>\n")
@@ -150,8 +159,6 @@ except OSError as file_error:
 #   files (see point 5 above), named 'departmentValue.xml', using the XML template                    #
 #   seen below.                                                                                       #
 #######################################################################################################
-
-# cycle through each dept col and write to that file the template data:
 
 try:
     # Write out top half of XML template to all department files
@@ -179,7 +186,7 @@ try:
 
     # Write data to individual 'child' (departmental) xml files from the .csv files generated from question 4
     for each_dept in dept_column:
-        csv_readfile = pd.read_csv(str(each_dept) + ".csv")
+        csv_readfile = pd.read_csv(str(each_dept) + ".csv", header=None)
         with open(str(each_dept) + ".xml", "+a") as out_file:
             for idx, each_row in csv_readfile.iterrows():
                 out_file.write("\t\t\t" + delimiter.join(each_row) + "\n")
@@ -199,17 +206,11 @@ except OSError as file_error:
 
 
 #######################################################################################################
-#   8. Generate a separate 'Child' XML file per 'Department' value, using the created CSV             #
-#   files (see point 5 above), named 'departmentValue.xml', using the XML template                    #
-#   seen below.                                                                                       #
+#  8.  When generating the XML files, within the XML node named "data", embed the                     #
+#      'Master' CSV file data or 'Child' CSV file data, as created (see points 7 & 8).                #
 #######################################################################################################
 
-message = "RW1haWwsU3VybmFtZSxGaXJzdE5hbWUsRGVwYXJ0bWVudCxDb3VudHJ5CmVtYWlsMSxTdXJuYW1lMSxGaXJzdG5hbWUxLFNhbGVzLElyZWxhbmQKZW1haWwyLFN1cm5hbWUyLEZpcnN0bmFtZTIsRmluYW5jZSxVSwplbWFpbDMsU3VybmFtZTMsRmlyc3RuYW1lMyxTYWxlcyxGcmFuY2UKZW1haWw0LFN1cm5hbWU0LEZpcnN0bmFtZTQsRW5naW5lZXJpbmcsR2VybWFueQplbWFpbDUsU3VybmFtZTUsRmlyc3RuYW1lNSxNYXJrZXRpbmcsVVNBCmVtYWlsNixTdXJuYW1lNixGaXJzdG5hbWU2LEZpbmFuY2UsSXJlbGFuZAplbWFpbDcsU3VybmFtZTcsRmlyc3RuYW1lNyxTYWxlcyxVSwplbWFpbDgsU3VybmFtZTgsRmlyc3RuYW1lOCxFbmdpbmVlcmluZyxGcmFuY2UKZW1haWw5LFN1cm5hbWU5LEZpcnN0bmFtZTksTWFya2V0aW5nLEdlcm1hbnkKZW1haWwxMCxTdXJuYW1lMTAsRmlyc3RuYW1lMTAsRmluYW5jZSxVU0EKZW1haWwxMSxTdXJuYW1lMTEsRmlyc3RuYW1lMTEsTWFya2V0aW5nLElyZWxhbmQKZW1haWwxMixTdXJuYW1lMTIsRmlyc3RuYW1lMTIsRW5naW5lZXJpbmcsVUsKZW1haWwxMyxTdXJuYW1lMTMsRmlyc3RuYW1lMTMsTWFya2V0aW5nLEZyYW5jZQplbWFpbDE0LFN1cm5hbWUxNCxGaXJzdG5hbWUxNCxNYXJrZXRpbmcsR2VybWFueQplbWFpbDE1LFN1cm5hbWUxNSxGaXJzdG5hbWUxNSxGaW5hbmNlLFVTQQplbWFpbDE2LFN1cm5hbWUxNixGaXJzdG5hbWUxNixTYWxlcyxJcmVsYW5kCmVtYWlsMTcsU3VybmFtZTE3LEZpcnN0bmFtZTE3LEVuZ2luZWVyaW5nLFVLCmVtYWlsMTgsU3VybmFtZTE4LEZpcnN0bmFtZTE4LE1hcmtldGluZyxGcmFuY2UKZW1haWwxOSxTdXJuYW1lMTksRmlyc3RuYW1lMTksRmluYW5jZSxHZXJtYW55CmVtYWlsMjAsU3VybmFtZTIwLEZpcnN0bmFtZTIwLFNhbGVzLFVTQQplbWFpbDIxLFN1cm5hbWUyMSxGaXJzdG5hbWUyMSxFbmdpbmVlcmluZyxJcmVsYW5kCmVtYWlsMjIsU3VybmFtZTIyLEZpcnN0bmFtZTIyLE1hcmtldGluZyxVSwplbWFpbDIzLFN1cm5hbWUyMyxGaXJzdG5hbWUyMyxGaW5hbmNlLEZyYW5jZQplbWFpbDI0LFN1cm5hbWUyNCxGaXJzdG5hbWUyNCxTYWxlcyxHZXJtYW55CmVtYWlsMjUsU3VybmFtZTI1LEZpcnN0bmFtZTI1LEVuZ2luZWVyaW5nLFVTQQplbWFpbDI2LFN1cm5hbWUyNixGaXJzdG5hbWUyNixNYXJrZXRpbmcsSXJlbGFuZAplbWFpbDI3LFN1cm5hbWUyNyxGaXJzdG5hbWUyNyxNYXJrZXRpbmcsVUsKZW1haWwyOCxTdXJuYW1lMjgsRmlyc3RuYW1lMjgsRmluYW5jZSxGcmFuY2UKZW1haWwyOSxTdXJuYW1lMjksRmlyc3RuYW1lMjksU2FsZXMsR2VybWFueQplbWFpbDMwLFN1cm5hbWUzMCxGaXJzdG5hbWUzMCxFbmdpbmVlcmluZyxVU0EKZW1haWwzMSxTdXJuYW1lMzEsRmlyc3RuYW1lMzEsTWFya2V0aW5nLElyZWxhbmQKZW1haWwzMixTdXJuYW1lMzIsRmlyc3RuYW1lMzIsRmluYW5jZSxVSwplbWFpbDMzLFN1cm5hbWUzMyxGaXJzdG5hbWUzMyxTYWxlcyxGcmFuY2UKZW1haWwzNCxTdXJuYW1lMzQsRmlyc3RuYW1lMzQsRW5naW5lZXJpbmcsR2VybWFueQplbWFpbDM1LFN1cm5hbWUzNSxGaXJzdG5hbWUzNSxNYXJrZXRpbmcsVVNBCmVtYWlsMzYsU3VybmFtZTM2LEZpcnN0bmFtZTM2LEZpbmFuY2UsSXJlbGFuZAplbWFpbDM3LFN1cm5hbWUzNyxGaXJzdG5hbWUzNyxTYWxlcyxVSwplbWFpbDM4LFN1cm5hbWUzOCxGaXJzdG5hbWUzOCxFbmdpbmVlcmluZyxGcmFuY2UKZW1haWwzOSxTdXJuYW1lMzksRmlyc3RuYW1lMzksTWFya2V0aW5nLEdlcm1hbnkKZW1haWw0MCxTdXJuYW1lNDAsRmlyc3RuYW1lNDAsVGVjaG5pY2FsIFNlcnZpY2VzLFVTQQplbWFpbDQxLFN1cm5hbWU0MSxGaXJzdG5hbWU0MSxGaW5hbmNlLElyZWxhbmQKZW1haWw0MixTdXJuYW1lNDIsRmlyc3RuYW1lNDIsRmluYW5jZSxVSwplbWFpbDQzLFN1cm5hbWU0MyxGaXJzdG5hbWU0MyxFbmdpbmVlcmluZyxGcmFuY2UKZW1haWw0NCxTdXJuYW1lNDQsRmlyc3RuYW1lNDQsTWFya2V0aW5nLEdlcm1hbnkKZW1haWw0NSxTdXJuYW1lNDUsRmlyc3RuYW1lNDUsRmluYW5jZSxVU0EKZW1haWw0NixTdXJuYW1lNDYsRmlyc3RuYW1lNDYsU2FsZXMsSXJlbGFuZAplbWFpbDQ3LFN1cm5hbWU0NyxGaXJzdG5hbWU0NyxFbmdpbmVlcmluZyxVSwplbWFpbDQ4LFN1cm5hbWU0OCxGaXJzdG5hbWU0OCxNYXJrZXRpbmcsRnJhbmNlCmVtYWlsNDksU3VybmFtZTQ5LEZpcnN0bmFtZTQ5LEZpbmFuY2UsR2VybWFueQplbWFpbDUwLFN1cm5hbWU1MCxGaXJzdG5hbWU1MCxTYWxlcyxVU0EK"
-#base64.encode(message)
-message_bytes = base64.b64decode(message)
-print("\n" + str(message_bytes))
-
-
-
-
+# Read in master data in binary, not text; "rb"
+master_csv_data = open(poppulo_csv_file, "rb").read()
+master_csv_encoded = base64.b64encode(master_csv_data)
 
